90e16b41f0dfaa7eedb69bff7316f920756de7bf
Benjamin Zinschitz <benjamin@formunauts.at>
2024-12-12 08:47:23 +0100

    crkbd: swap single quote and backtick position

--- keyboards/crkbd/keymaps/benjaminimal/keymap.c
+++ keyboards/crkbd/keymaps/benjaminimal/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
     [SYM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-            KC_TRNS, KC_TILD, KC_PIPE, KC_AT, KC_GRV, KC_QUOT, KC_NO, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_TRNS,
+            KC_TRNS, KC_TILD, KC_PIPE, KC_AT, KC_QUOT, KC_GRV, KC_NO, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
             KC_TRNS, KC_EXLM, KC_DQUO, KC_NUHS, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
