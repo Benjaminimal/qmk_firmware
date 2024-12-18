56bedf8f3c0a45d65e7953a67bc671df034f1b9b
Benjamin Zinschitz <benjamin@formunauts.at>
2024-12-18 01:35:34 +0100

    crkbd: put mod taps on home row

--- keyboards/crkbd/keymaps/benjaminimal/keymap.c
+++ keyboards/crkbd/keymaps/benjaminimal/keymap.c
@@ -16,18 +16,14 @@
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                       //`--------------------------'  `--------------------------'
 
-/* TODO
- *  - Mod-Tap not working for shifted keys
- */
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BSE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
             KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+            KC_CAPS, LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN), KC_ENT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            KC_NUBS, LCTL_T(KC_Z), LGUI_T(KC_X), LALT_T(KC_C), LSFT_T(KC_V), KC_B, KC_N, RSFT_T(KC_M), RALT_T(KC_COMM), RGUI_T(KC_DOT), RCTL_T(KC_SLSH), KC_MINS,
+            KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         OSL(NUM), KC_SPC, OSL(FNC), OSL(MTA), OSL(SYM), OSL(NAV)
                                       //`--------------------------'  `--------------------------'
@@ -36,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
             KC_TRNS, KC_TILD, KC_PIPE, KC_NUHS, KC_DLR, KC_PERC, KC_NO, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            KC_TRNS, KC_EXLM, KC_DQUO, KC_AT, KC_QUOT, KC_GRV, KC_CIRC, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_TRNS,
+            KC_TRNS, LCTL_T(KC_EXLM), LGUI_T(KC_DQUO), LALT_T(KC_AT), RSFT_T(KC_QUOT), KC_GRV, KC_CIRC, LSFT_T(KC_LPRN), RALT_T(KC_RPRN), RGUI_T(KC_LCBR), RCTL_T(KC_RCBR), KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
             QK_LLCK, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_NO, KC_NO, KC_RSFT, KC_RALT, KC_RGUI, KC_RCTL, QK_LLCK,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -47,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
             KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSLS, KC_PMNS, KC_PAST, KC_PPLS, KC_EQL, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
+            KC_TRNS, LCTL_T(KC_1), LGUI_T(KC_2), LALT_T(KC_3), RSFT_T(KC_4), KC_5, KC_6, LSFT_T(KC_7), RALT_T(KC_8), RGUI_T(KC_9), RCTL_T(KC_0), KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
             QK_LLCK, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_NO, KC_NO, KC_RSFT, RALT_T(KC_COMM), RGUI_T(KC_DOT), KC_RCTL, QK_LLCK,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -58,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
             KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_DEL, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_INS, KC_TRNS,
+            KC_TRNS, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_NO, KC_LEFT, RSFT_T(KC_DOWN), RALT_T(KC_UP), RGUI_T(KC_RGHT), RCTL_T(KC_INS), KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            QK_LLCK, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_NO, KC_NO, KC_RSFT, KC_RALT, KC_RGUI, KC_RCTL, QK_LLCK,
+            QK_LLCK, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QK_LLCK,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
@@ -69,9 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
             KC_TRNS, KC_ESC, KC_NUM, KC_SCRL, KC_PAUS, KC_F6, KC_F7, KC_APP, KC_PSCR, KC_NO, KC_NO, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,
+            KC_TRNS, LCTL_T(KC_F1), LGUI_T(KC_F2), LALT_T(KC_F3), LSFT_T(KC_F4), KC_F5, KC_F8, RSFT_T(KC_F9), RALT_T(KC_F10), RGUI_T(KC_F11), RCTL_T(KC_F12), KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            QK_LLCK, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_NO, KC_NO, KC_RSFT, KC_RALT, KC_RGUI, KC_RCTL, QK_LLCK,
+            QK_LLCK, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QK_LLCK,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
@@ -89,6 +85,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         )
 };
 
+
+/**
+ * This code is needed to enable mod tap for non basic key codes
+ * https://docs.qmk.fm/mod_tap#intercepting-mod-taps
+ */
+
+#define ENABLE_MOD_TAP(mod_tap, key_code) \
+        case mod_tap(key_code): \
+            if (record->tap.count && record->event.pressed) { \
+                tap_code16(key_code); \
+                return false; \
+            } \
+            break;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        ENABLE_MOD_TAP(LCTL_T, KC_EXLM)
+        ENABLE_MOD_TAP(LGUI_T, KC_DQUO)
+        ENABLE_MOD_TAP(LALT_T, KC_AT)
+        ENABLE_MOD_TAP(LSFT_T, KC_LPRN)
+        ENABLE_MOD_TAP(RALT_T, KC_RPRN)
+        ENABLE_MOD_TAP(RGUI_T, KC_LCBR)
+        ENABLE_MOD_TAP(RCTL_T, KC_RCBR)
+        ENABLE_MOD_TAP(LCTL_T, KC_DQUO)
+    }
+    return true;
+}
 
 
 #ifdef OTHER_KEYMAP_C
